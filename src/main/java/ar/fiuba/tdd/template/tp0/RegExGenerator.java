c42a2a006c0878a552b429bfc28d882244582c0c
seba0703 <sebastian.a.rocha@hotmail.com>
2016-03-18 11:22:12 -0300

    Generator link to Token class and Parser Class

--- src/main/java/ar/fiuba/tdd/template/tp0/RegExGenerator.java
+++ src/main/java/ar/fiuba/tdd/template/tp0/RegExGenerator.java
@@ -1,24 +1,37 @@
 package ar.fiuba.tdd.template.tp0;
 
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 
 public class RegExGenerator {
     // TODO: Uncomment this field
-    //private int maxLength;
+    private int maxLength;
+    private RegExParser parser;
+    private List<RegExToken> tokens;
+    private ArrayList<String> matchedStrings;
 
-    //public RegExGenerator(int maxLength) {
-    //    this.maxLength = maxLength;
-    //}
+    public RegExGenerator(int maxLength) {
+        this.maxLength = maxLength;
+        this.parser = new RegExParser();
+    }
+
+    private String matchString(){
+        String matchedString = "";
+        for (RegExToken token:tokens) {
+            matchedString = matchedString + token.make();
+        }
+        return matchedString;
+    }
 
     // TODO: Uncomment parameters
-    public List<String> generate(/*String regEx, int numberOfResults*/) {
-        return new ArrayList<String>() {
-            {
-                add("a");
-                add("b");
-                add("c");
-            }
-        };
+    public List<String> generate(String regEx, int numberOfResults) {
+        tokens = parser.parseString(regEx);
+        matchedStrings = new ArrayList<String>();
+        for (int i = 0; i < numberOfResults; i++) {
+            matchedStrings.add(matchString());
+        }
+        return matchedStrings;
+
     }
 }
